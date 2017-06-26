77d0bac062e59b8fa889be0ad22b79f6c0ba030e
aracwong <aracwong@163.com>
2017-06-26 22:00:00 +0800

    Initial commit

---
+++ learn-logback/src/main/java/com/kotall/learn/logback/DemoService.java
@@ -0,0 +1,21 @@
+package com.kotall.learn.logback;/**
+ * Created by Administrator on 2017/6/26 0026.
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author aracwong
+ * @version 1.0.0
+ * @email
+ * @datetime 2017/6/26 0026 下午 8:47
+ */
+public class DemoService {
+    private static final Logger logger = LoggerFactory.getLogger(DemoService.class);
+
+    public String demo() {
+        logger.info("---demo---");
+        return "this is demo!";
+    }
+}
---
+++ learn-logback/src/test/java/com/kotall/learn/logback/App.java
@@ -0,0 +1,26 @@
+package com.kotall.learn.logback;/**
+ * Created by Administrator on 2017/6/26 0026.
+ */
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author aracwong
+ * @version 1.0.0
+ * @email
+ * @datetime 2017/6/26 0026 下午 4:50
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring-context.xml");
+        ctx.start();
+        String[] names = ctx.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
+        DemoService demoService = (DemoService) ctx.getBean("demoService");
+        demoService.demo();
+
+        Thread.sleep(100000 * 60 * 60 * 1000L);
+    }
+}
