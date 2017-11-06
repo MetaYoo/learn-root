06e4e74a9884957111896a2c699fa470a768fd09
aracwong <aracwong@163.com>
2017-11-06 22:17:34 +0800

    update learn-shiro

--- learn-shiro/learn-shiro-ini/src/test/java/com/kotall/learn/ini/ShiroIniTest.java
+++ learn-shiro/learn-shiro-ini/src/test/java/com/kotall/learn/ini/ShiroIniTest.java
@@ -2,7 +2,10 @@ package com.kotall.learn.ini;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.codec.Base64;
 import org.apache.shiro.env.DefaultEnvironment;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.subject.Subject;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,8 +14,11 @@ public class ShiroIniTest {
 
     @Test
     public void ini() {
-        org.apache.shiro.mgt.SecurityManager securityManager = new DefaultEnvironment().getSecurityManager();
-        SecurityUtils.setSecurityManager(securityManager);
+        DefaultEnvironment environment = new DefaultEnvironment();
+
+        environment.setSecurityManager(new DefaultSecurityManager(new IniRealm("classpath:shiro.ini")));
+
+        SecurityUtils.setSecurityManager(environment.getSecurityManager());
 
         Subject subject = SecurityUtils.getSubject();
 
@@ -21,5 +27,23 @@ public class ShiroIniTest {
         subject.login(token);
 
         Assert.assertEquals(true, subject.isAuthenticated());
+
+        subject.logout();
+
+        Assert.assertEquals(false, subject.isAuthenticated());
+
+    }
+
+    @Test
+    public void code() {
+        String str = "hello world";
+        String codeStr = Base64.encodeToString(str.getBytes());
+
+        System.out.println(codeStr);
+
+        String str2 = new String(Base64.decodeToString(codeStr.getBytes()));
+
+        Assert.assertEquals(str, str2);
+
     }
 }
