a3b3313a0628b55a509814a0ed67bf5c97e396a4
aracwong <aracwong@163.com>
2018-03-08 22:37:57 +0800

    learn-springboot更新

---
+++ learn-springboot/src/main/java/com/kotall/learn/springboot/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.kotall.learn.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : aracwong
+ * @version : 1.0.0
+ * @date : 2018/3/8 0008 下午 10:13
+ */
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping(value = "/hello", produces = "application/json")
+    public String hello() {
+        return "hello world";
+    }
+}
