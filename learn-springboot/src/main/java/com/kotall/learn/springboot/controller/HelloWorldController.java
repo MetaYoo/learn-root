51a821f79adb7cc8bbe1b42d92df914508bf62da
skyarac <skyarac@sina.com>
2018-03-10 15:54:05 +0800

    update learn-springboot

--- learn-springboot/src/main/java/com/kotall/learn/springboot/controller/HelloWorldController.java
+++ learn-springboot/src/main/java/com/kotall/learn/springboot/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.kotall.learn.springboot.controller;
 
+import com.kotall.learn.springboot.configuration.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    AppConfig appConfig;
+
     @RequestMapping(value = "/hello", produces = "application/json")
     public String hello() {
+        System.out.println("========name=" + appConfig.getName() + "; age=" + appConfig.getAge());
         return "{'msg':'hello world !'}";
     }
 }
