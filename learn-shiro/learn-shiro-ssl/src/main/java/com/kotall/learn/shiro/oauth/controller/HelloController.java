29a2ac7bdb8e2f2c238abfb5d8537031488b7e4f
aracwong <aracwong@163.com>
2018-04-14 15:23:23 +0800

    update  springboot ssl

---
+++ learn-shiro/learn-shiro-ssl/src/main/java/com/kotall/learn/shiro/oauth/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.kotall.learn.shiro.oauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : aracwong
+ * @version : 1.0.0
+ * @date : 2018/4/14 0014 下午 2:52
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "hello world";
+    }
+}
