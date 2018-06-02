c5506a9dc50103db3d296114e0724b04007cd86a
aracwong <aracwong@163.com>
2018-06-02 18:24:41 +0800

    springboot update

--- learn-springboot/src/main/java/com/kotall/learn/springboot/App.java
+++ learn-springboot/src/main/java/com/kotall/learn/springboot/App.java
@@ -1,16 +1,16 @@
 package com.kotall.learn.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author : aracwong
  * @version : 1.0.0
  * @date : 2018/3/8 0008 下午 10:13
  */
-@ComponentScan
-@EnableAutoConfiguration
+//@ComponentScan
+//@EnableAutoConfiguration
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
