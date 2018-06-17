405ed8e82a1bb973eb26a08296f51ae528fbfbeb
aracwong <aracwong@163.com>
2018-06-17 19:25:37 +0800

    dp update

---
+++ learn-dp/learn-dp-proxy-live/src/main/java/com/kotall/learn/proxy/service/UserService.java
@@ -0,0 +1,10 @@
+package com.kotall.learn.proxy.service;
+
+/**
+ * @author zpwang
+ * @version 1.0.0
+ */
+public interface UserService {
+
+    void addUser();
+}
---
+++ learn-dp/learn-dp-proxy-live/src/main/java/com/kotall/learn/proxy/service/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.kotall.learn.proxy.service;
+
+/**
+ * @author zpwang
+ * @version 1.0.0
+ */
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public void addUser() {
+        System.out.println("=======添加用户");
+    }
+
+}
