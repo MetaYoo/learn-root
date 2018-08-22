902d3eaea1b771ceac92cd032a4390c0fb511643
aracwong <aracwong@163.com>
2018-08-22 22:15:57 +0800

    update zookeeper demo

--- learn-zk/learn-zk-demo/src/main/java/com/kotall/learn/zk/ZkDemo.java
+++ learn-zk/learn-zk-demo/src/main/java/com/kotall/learn/zk/ZkDemo.java
@@ -3,9 +3,9 @@ package com.kotall.learn.zk;
 import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
 
-import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author : zpwang
@@ -16,7 +16,7 @@ public class ZkDemo {
 
     private String connectStr = "localhost:2181";
     private ZooKeeper zkClient;
-    private CountDownLatch connectedSemaPhore = new CountDownLatch(1);
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
 
     public static void main(String[] args) throws Exception {
         ZkDemo demo = new ZkDemo();
@@ -26,6 +26,10 @@ public class ZkDemo {
         // 2. 创建根节点
         demo.createNode("/p");
         // 3. 创建子节点
+        TimeUnit.SECONDS.sleep(3);
+        // 4. 创建子节点
+        demo.createNode("/p/p1");
+        TimeUnit.SECONDS.sleep(3);
     }
 
     private void createConnection() {
@@ -34,7 +38,7 @@ public class ZkDemo {
                 this.zkClient.close();
             }
             zkClient = new ZooKeeper(connectStr, 1000, new ZkWatcher());
-            connectedSemaPhore.await();
+            countDownLatch.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -58,19 +62,39 @@ public class ZkDemo {
 
     class ZkWatcher implements Watcher{
 
+        private AtomicInteger atomicInteger = new AtomicInteger();
+
         @Override
         public void process(WatchedEvent event) {
             Event.EventType eventType = event.getType();
             Event.KeeperState state = event.getState();
+            String logPrefix = "【Watcher " + atomicInteger.getAndIncrement() +"】, state=" + state + ", eventType=" + eventType;
+            System.out.println(logPrefix);
             if (Event.KeeperState.SyncConnected.equals(state)) {
                 if (Event.EventType.None == eventType) {
-                    System.out.println("创建连接成功！");
+                    System.out.println(" 创建连接成功！");
+                    countDownLatch.countDown();
                 }
-
-                if (Event.EventType.NodeCreated.equals(state)) {
-                    System.out.println("创建节点事件！");
+                if (Event.EventType.NodeCreated.equals(eventType)) {
+                    System.out.println(" 创建节点事件！");
                 }
-            }
+                if (Event.EventType.NodeDeleted.equals(eventType)) {
+                    System.out.println(" 删除节点事件！");
+                }
+                if (Event.EventType.NodeDataChanged.equals(eventType)) {
+                    System.out.println(" 节点数据变化事件！");
+                }
+                if (Event.EventType.NodeChildrenChanged.equals(eventType)) {
+                    System.out.println(" 子节点变化事件！");
+                }
+            } else if (Event.KeeperState.Disconnected.equals(state)) {
+                System.out.println(logPrefix + " 与zk服务器断开连接！");
+            } else if (Event.KeeperState.AuthFailed.equals(state)) {
+                System.out.println(logPrefix + " 权限检查失败！");
+            } else if (Event.KeeperState.Expired.equals(state)) {
+                System.out.println(logPrefix + " 会话失效");
+            } else;
+            System.out.println("---------------------------------------------");
 
 
         }
