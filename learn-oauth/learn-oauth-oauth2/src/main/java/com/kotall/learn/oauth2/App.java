bca17310d1365073dada8ac66d1a84c748114831
aracwong <aracwong@163.com>
2018-08-26 15:21:26 +0800

    update oauth2 demo

---
+++ learn-oauth/learn-oauth-oauth2/src/main/java/com/kotall/learn/oauth2/App.java
@@ -0,0 +1,15 @@
+package com.kotall.learn.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zpwang
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args).start();
+    }
+}
