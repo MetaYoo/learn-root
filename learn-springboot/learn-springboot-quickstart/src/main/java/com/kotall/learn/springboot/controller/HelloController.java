6a9099943468f5c094b54a6eff0cf4f133b25128
aracwong <aracwong@163.com>
2018-09-01 08:33:40 +0800

    update springboot oauth2 demo

---
+++ learn-springboot/learn-springboot-quickstart/src/main/java/com/kotall/learn/springboot/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.kotall.learn.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zpwang
+ * @version 1.0.0
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/{param}")
+    public String hello(@PathVariable("param") String param) {
+        return param;
+    }
+}
