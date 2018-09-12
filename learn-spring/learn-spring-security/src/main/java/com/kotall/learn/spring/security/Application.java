e1846ddbfd3f5ddb219a8a2cf07466c115aacb8a
aracwong <aracwong@163.com>
2018-09-12 22:49:09 +0800

    update spring security demo

---
+++ learn-spring/learn-spring-security/src/main/java/com/kotall/learn/spring/security/Application.java
@@ -0,0 +1,16 @@
+package com.kotall.learn.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zpwang
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ learn-spring/learn-spring-security/src/main/java/com/kotall/learn/spring/security/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.kotall.learn.spring.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author zpwang
+ * @version 1.0.0
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
