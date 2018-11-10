774f13141ee2582885922a7a329d57efed92ccc4
aracwong <aracwong@163.com>
2018-11-10 22:02:08 +0800

    add learn-demo-login-springsecurity-memory module

---
+++ learn-demo/learn-demo-login/learn-demo-login-springsecurity-memory/src/main/java/com/kotall/learn/demo/login/filter/BeforeLoginFilter.java
@@ -0,0 +1,22 @@
+package com.kotall.learn.demo.login.filter;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * @author zpwang
+ * @version 1.0.0
+ */
+public class BeforeLoginFilter extends GenericFilterBean {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("This is a filter before UsernamePasswordAuthenticationFilter.");
+        // 继续调用 Filter 链
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
