a25c9cc67c2e12e28602cf534739da0f40e33c41
aracwong <aracwong@163.com>
2018-12-24 17:36:50 +0800

    算法学习

---
+++ learn-arithmetic/src/main/java/com/kotall/learn/arithmetic/HeapSort.java
@@ -0,0 +1,29 @@
+package com.kotall.learn.arithmetic;
+
+public class HeapSort {
+
+    public static void main(String[] args) {
+        int[] a = {6,2,8,4,7,9, 11,1,3,10,5};
+        HeapSort heapSort = new HeapSort();
+        heapSort.sort(a);
+        heapSort.showHeap(a);
+    }
+
+    private void showHeap(int[] a) {
+
+    }
+
+    public void sort(int[] source) {
+        // 1. 构建(调整)大顶堆
+        // 2. 调整堆
+
+    }
+
+    private void adjustHeap(int[] source) {
+
+    }
+
+    public void swap(int[] source) {
+
+    }
+}
