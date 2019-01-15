2e28c34de7610707b0f533b5116511676e47716a
aracwong@163.com <haiyasheji@163.com>
2019-01-15 22:22:57 +0800

    spring namespace update

--- learn-spring/learn-spring-namespace/src/test/java/com/kotall/learn/AppTest.java
+++ learn-spring/learn-spring-namespace/src/test/java/com/kotall/learn/AppTest.java
@@ -1,9 +1,6 @@
 package com.kotall.learn;
 
-import com.kotall.learn.spring.service.BlogService;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,11 +11,4 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ContextConfiguration("classpath:applicationContext.xml")
 public class AppTest {
 
-    @Autowired
-    private BlogService blogService;
-
-    @Test
-    public void post() {
-        this.blogService.post("20190107");
-    }
 }
