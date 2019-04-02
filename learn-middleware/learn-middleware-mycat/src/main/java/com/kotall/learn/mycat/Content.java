90e09d2a38464d516173e7a2c7febdaa60dd2e97
zpwang <aracwong@163.com>
2019-04-02 21:42:09 +0800

    update mycat demo

--- learn-middleware/learn-middleware-mycat/src/main/java/com/kotall/learn/mycat/Content.java
+++ learn-middleware/learn-middleware-mycat/src/main/java/com/kotall/learn/mycat/Content.java
@@ -10,14 +10,19 @@ import javax.persistence.*;
  * @author: zpwang
  * @time: 2019/3/28 22:26
  */
-@Data
-@Entity
+
 @Table(name = "content")
+@Data
 public class Content {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "txid")
     private String txId;
+
     private String title;
+
     private String content;
 }
