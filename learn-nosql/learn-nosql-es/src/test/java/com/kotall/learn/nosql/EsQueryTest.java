9d9b479c6dfd71ac51ce7ab456ec4b03ecb2877e
skyarac <haiyasheji@163.com>
2019-05-09 22:08:43 +0800

    es demo

--- learn-nosql/learn-nosql-es/src/test/java/com/kotall/learn/nosql/EsQueryTest.java
+++ learn-nosql/learn-nosql-es/src/test/java/com/kotall/learn/nosql/EsQueryTest.java
@@ -1,6 +1,11 @@
 package com.kotall.learn.nosql;
 
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +26,37 @@ public class EsQueryTest {
     private RestHighLevelClient client;
 
     @Test
-    public void testQuery1() {
+    public void testQuery1() throws Exception {
+        SearchRequest request = new SearchRequest();
 
+        SearchSourceBuilder searchSourceBuilder = SearchSourceBuilder.searchSource();
+        searchSourceBuilder.query(QueryBuilders.termQuery("name", "test1"));
+        request.source(searchSourceBuilder);
+
+        SearchResponse response = this.client.search(request, RequestOptions.DEFAULT);
+        System.out.println(response);
     }
+
+    @Test
+    public void testQuery2() throws Exception {
+        SearchRequest request = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = SearchSourceBuilder.searchSource();
+        searchSourceBuilder.query(QueryBuilders.matchQuery("name", "test2"));
+        request.source(searchSourceBuilder);
+        SearchResponse response = this.client.search(request, RequestOptions.DEFAULT);
+        System.out.println(response);
+    }
+
+    @Test
+    public void testQuery3() throws Exception {
+        SearchRequest request = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = SearchSourceBuilder.searchSource();
+        searchSourceBuilder.query(QueryBuilders.boolQuery());
+
+        request.source(searchSourceBuilder);
+        SearchResponse response = this.client.search(request, RequestOptions.DEFAULT);
+        System.out.println(response);
+    }
+
+
 }
