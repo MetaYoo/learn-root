b01d33c5fc8dc44c0f777e0e90a687563aff9808
skyarac <haiyasheji@163.com>
2019-07-08 22:23:09 +0800

    springcloud Prometheus 整合

--- learn-springcloud/learn-springcloud-samples/learn-springcloud-samples-order/src/main/java/com/kotall/learn/cloud/samples/OrderServer.java
+++ learn-springcloud/learn-springcloud-samples/learn-springcloud-samples-order/src/main/java/com/kotall/learn/cloud/samples/OrderServer.java
@@ -1,5 +1,9 @@
 package com.kotall.learn.cloud.samples;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * 描述：
  *
