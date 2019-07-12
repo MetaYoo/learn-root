99d910581b38782c806a342d02db618ce41d2875
skyarac <haiyasheji@163.com>
2019-07-12 20:40:49 +0800

    sso demo

---
+++ learn-auth/learn-sso/learn-sso-server/src/main/java/com/kotall/learn/sso/SSOServer.java
@@ -0,0 +1,10 @@
+package com.kotall.learn.sso;
+
+/**
+ * Hello world!
+ */
+public class SSOServer {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
