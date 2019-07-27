c33076fd0033121d391f97ad38bfa100729ae4e4
skyarac <haiyasheji@163.com>
2019-07-27 14:07:55 +0800

    redis 集群演示

--- learn-jvm/learn-jvm-thread/src/test/java/com/kotall/learn/threadlocal/ThreadLocalTest.java
+++ learn-jvm/learn-jvm-thread/src/test/java/com/kotall/learn/threadlocal/ThreadLocalTest.java
@@ -2,6 +2,8 @@ package com.kotall.learn.threadlocal;
 
 import org.junit.Test;
 
+import java.util.concurrent.*;
+
 public class ThreadLocalTest {
 
     @Test
@@ -15,6 +17,29 @@ public class ThreadLocalTest {
     public void testThreadLocal() {
         ThreadLocal threadLocal = new ThreadLocal();
         threadLocal.set("tttt");
+    }
+
+    @Test
+    public void testMultiThread() throws Exception {
+        ExecutorService executorService = new ThreadPoolExecutor(1, 3, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue(1200));
+        for (int i=0; i < 1000; i++) {
+            executorService.submit(new Task(i));
+        }
+
+        TimeUnit.SECONDS.sleep(100000);
+
+    }
+
+
+    class Task implements Runnable {
+        int i;
+        public Task(int i) {
+            this.i = i;
+        }
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + i);
+        }
     }
 
 }
