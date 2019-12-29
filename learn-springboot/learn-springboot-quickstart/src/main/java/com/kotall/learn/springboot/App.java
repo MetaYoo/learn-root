e43d1bbf13fa1ae03d5501d25b71945ae8268f69
skyarac <haiyasheji@163.com>
2019-12-29 22:04:23 +0800

    添加 cors配置

--- learn-springboot/learn-springboot-quickstart/src/main/java/com/kotall/learn/springboot/App.java
+++ learn-springboot/learn-springboot-quickstart/src/main/java/com/kotall/learn/springboot/App.java
@@ -7,15 +7,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
-public class App {
+public class App implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args).start();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(30 * 1000);
     }
 
     /**
