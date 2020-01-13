2c6af132d4b8137b581b83b75f01d923b35ff1fc
zpwang <wangzhongping@ycmedia.cn>
2020-01-13 20:12:27 +0800

    docker demo

---
+++ learn-diy/learn-diy-mybatis/src/main/java/com/kotall/learn/diy/bo/Blog.java
@@ -0,0 +1,17 @@
+package com.kotall.learn.diy.bo;
+
+import lombok.Data;
+
+/**
+ * desc:
+ *
+ * @author zpwang
+ * @date 2020/1/13 10:16
+ * @since 1.0.0
+ */
+@Data
+public class Blog {
+    private Integer id;
+    private String title;
+    private String content;
+}
---
+++ learn-diy/learn-diy-mybatis/src/main/java/com/kotall/learn/diy/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * desc:
+ *
+ * @author zpwang
+ * @date 2020/1/13 10:15
+ * @since 1.0.0
+ */
+package com.kotall.learn.diy;
