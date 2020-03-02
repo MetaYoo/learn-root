b14cf4c10428faf5631d434a12a407329111f9c6
zpwang <wangzhongping@ycmedia.cn>
2020-03-02 15:37:35 +0800

    JOL

---
+++ learn-jvm/learn-jvm-lock/src/main/java/com/kotall/learn/cachelinepadding/CacheLinePaddingTest.java
@@ -0,0 +1,24 @@
+package com.kotall.learn.cachelinepadding;
+
+/**
+ * desc:
+ * line-cache-padding 技术
+ * 缓存行填充技术
+ *
+ * @author zpwang
+ * @date 2020/3/2 14:05
+ * @since 1.0.0
+ */
+public class CacheLinePaddingTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    static class T {
+        public int x;
+    }
+}
