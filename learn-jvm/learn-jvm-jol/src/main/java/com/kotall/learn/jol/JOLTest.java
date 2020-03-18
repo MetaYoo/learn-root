51f2ed56a00f9d71a6364552270ccb8d8293c8c4
zpwang <wangzhongping@ycmedia.cn>
2020-03-18 16:46:56 +0800

    线程通信 demo

--- learn-jvm/learn-jvm-jol/src/main/java/com/kotall/learn/jol/JOLTest.java
+++ learn-jvm/learn-jvm-jol/src/main/java/com/kotall/learn/jol/JOLTest.java
@@ -1,7 +1,18 @@
 package com.kotall.learn.jol;
 
+import org.openjdk.jol.info.ClassLayout;
+
+/**
+ * @author wzp
+ * @date 2020-03-08
+ */
 public class JOLTest {
     public static void main(String[] args) {
         Object o = new Object();
+        System.out.println(ClassLayout.parseInstance(o).toPrintable());
+
+        synchronized (o) {
+            System.out.println(ClassLayout.parseInstance(o).toPrintable());
+        }
     }
 }
