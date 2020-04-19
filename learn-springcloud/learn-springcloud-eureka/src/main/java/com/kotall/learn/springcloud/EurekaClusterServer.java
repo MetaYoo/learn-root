3509d879412204ff9a6e7b9f99884df7c6590563
aracwong <aracwong@163.com>
2020-04-19 14:17:06 +0800

    update springcloud eureka demo

---
+++ learn-springcloud/learn-springcloud-eureka/src/main/java/com/kotall/learn/springcloud/EurekaClusterServer.java
@@ -0,0 +1,13 @@
+package com.kotall.learn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaClusterServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClusterServer.class, args);
+    }
+}
